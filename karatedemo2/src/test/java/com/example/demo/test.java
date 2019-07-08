9c38aa393a43f5384fd51d82e11c2c324c49560f
swathi <swathi@GISI-0NVH5P.gisi.com>
2019-07-08 14:37:48 +0530

    second commit

--- karatedemo2/src/test/java/com/example/demo/test.java
+++ karatedemo2/src/test/java/com/example/demo/test.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import com.intuit.karate.junit4.Karate;
 
 import cucumber.api.CucumberOptions;
-//@CucumberOptions(features="classpath:test.feature",plugin= {"html:target/test-report","junit:target/junit-xml-report.xml","json:target/json-report.json"})
+
 @RunWith(Karate.class)
 public class test {
 
